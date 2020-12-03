c76734ede1b1d0e584bdb63d0e3c936770c53965
Saddaf Afrin Khan <saddaf88@gmail.com>
2020-12-03 04:25:14 +0100

    (Python-Audio-Book) - Added pdf reader package

--- Python/AudioBook/main.py
+++ Python/AudioBook/main.py
@@ -1,4 +1,11 @@
 import pyttsx3
+import PyPDF2
+
+pdfBook = open('book.pdf', 'rb')
+bookReader = PyPDF2.PdfFileReader(pdfBook)
+pageNumber = bookReader.numPages
+print('your book has ' + str(pageNumber) + ' pages')
+
 speaker = pyttsx3.init()
 speaker.say("Hello world")
 speaker.runAndWait()
