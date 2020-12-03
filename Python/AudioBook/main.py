7c5a35f9fd030328ba4950dd24b81be1ca77cd65
Saddaf Afrin Khan <saddaf88@gmail.com>
2020-12-03 11:42:18 +0100

    (Python-Audio-Book) - Reading just one page of a book

--- Python/AudioBook/main.py
+++ Python/AudioBook/main.py
@@ -6,6 +6,10 @@ bookReader = PyPDF2.PdfFileReader(pdfBook)
 pageNumber = bookReader.numPages
 print('your book has ' + str(pageNumber) + ' pages')
 
+page = bookReader.getPage(0)
+textOfPage = page.extractText()
+print(textOfPage)
+
 speaker = pyttsx3.init()
-speaker.say("Hello world")
+speaker.say(textOfPage)
 speaker.runAndWait()
